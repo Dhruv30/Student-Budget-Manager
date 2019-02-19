d4f385e3cff26084b4db41a91b7d80708e52bad3
D-Harsh <harsh200014@gmail.com>
2019-02-19 17:30:58 -0500

    Firebase Setup

--- app/src/main/java/com/example/goose/studentbudgetmanager/StartUp.java
+++ app/src/main/java/com/example/goose/studentbudgetmanager/StartUp.java
@@ -3,11 +3,27 @@ package com.example.goose.studentbudgetmanager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class StartUp extends AppCompatActivity {
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class StartUp extends AppCompatActivity {
+    private FirebaseAnalytics mFirebaseAnalytics;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_up);
+
     }
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+    }
+
+
 }
